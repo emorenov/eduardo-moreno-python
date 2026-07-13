1639e80425a7e64416a1910080919798551d29c3
emorenov <92907767+emorenov@users.noreply.github.com>
2026-07-13 17:54:16 -0500

    Updates to budget calculator script

--- budget-calculator.py
+++ budget-calculator.py
@@ -15,43 +15,31 @@
 # Collect information for user
 # Variables - store information
 
-budget = 600.0
-
 name = input("What's your name: ")
-print(f"Hello! {name}")
+print(f"\nHello! {name}\n")
 
-streaming_service_name_1 = input("What is streaming service 1? ")
-print(f"DEBUG - name: {streaming_service_name_1}, Type: {type(streaming_service_name_1)}")
+budget = float(input("What's your budget? "))
+
+streaming_service_name_1 = input("\nWhat is streaming service 1? ")
 
 streaming_service_price_1 = float(input(f"What is the price for {streaming_service_name_1}? "))
-print(f"DEBUG - value: {streaming_service_price_1}, Type:{type(streaming_service_price_1)}")
 
-
-streaming_service_name_2 = input("What is streaming service 2? ")
-print(f"DEBUG - name: {streaming_service_name_2}, Type: {type(streaming_service_name_2)}")
+streaming_service_name_2 = input("\nWhat is streaming service 2? ")
 
 streaming_service_price_2 = float(input(f"What is the price for {streaming_service_name_2}? "))
-print(f"DEBUG - value: {streaming_service_price_2}, Type:{type(streaming_service_price_2)}")
 
-
-streaming_service_name_3 = input("What is streaming service 3? ")
-print(f"DEBUG - name: {streaming_service_name_3}, Type: {type(streaming_service_name_3)}")
+streaming_service_name_3 = input("\nWhat is streaming service 3? ")
 
 streaming_service_price_3 = float(input(f"What is the price for {streaming_service_name_3}? "))
-print(f"DEBUG - value: {streaming_service_price_3}, Type:{type(streaming_service_price_3)}")
 
-months = input("How many months do you want to calculate? ")
-print(f"DEBUG - value: {months}, Type:{type(months)}")
+months = input("\nHow many months do you want to calculate? ")
 
 # Math
 monthly_total = streaming_service_price_1 + streaming_service_price_2 + streaming_service_price_3
-print(f"DEBUG - value: {monthly_total}, Type:{type(monthly_total)}")
 
 grand_total = monthly_total * float(months)
-print(f"DEBUG - value: {grand_total}, Type:{type(grand_total)}")
 
 is_over_budget = grand_total > budget
-print(f"DEBUG - value: {is_over_budget}, Type:{type(is_over_budget)}")
 
 
 # Print a report
